853119642c41085d6028985fd2167456f4406a90
Damien Gonot <damien.gonot@gmail.com>
2021-01-22 09:07:50 -0500

    limit top 5 posts on home

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Link from "next/link";
 
 import fs from "fs";
@@ -21,7 +20,7 @@ export default function Home({ posts }) {
         Recent blog articles
       </div>
       <div className="pl-4">
-        {posts.sort((p1, p2) => new Date(p2.date) - new Date(p1.date)).map(({ frontmatter: { title, description, date }, slug }) => (
+        {posts.map(({ frontmatter: { title, description, date }, slug }) => (
           <article key={slug}>
             <header>
               <h3 className="mb-0 mt-4">
@@ -43,7 +42,7 @@ export default function Home({ posts }) {
 export async function getStaticProps() {
   const files = fs.readdirSync(`${process.cwd()}/content/posts`);
 
-  const posts = files.map((filename) => {
+  const allPosts = files.map((filename) => {
     const markdownWithMetadata = fs
       .readFileSync(`content/posts/${filename}`)
       .toString();
@@ -64,6 +63,8 @@ export async function getStaticProps() {
       frontmatter,
     };
   });
+
+  const posts = allPosts.sort((p1, p2) => new Date(p2.date) - new Date(p1.date)).slice(0, 5);
 
   return {
     props: {

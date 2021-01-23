c690f1b197905728de7ce215f522b09fef0eab99
Damien Gonot <damien.gonot@gmail.com>
2021-01-22 22:27:51 -0500

    better intro

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,8 @@ export default function Home({ posts }) {
           Introduction
         </div>
         <div className="pl-4 mt-4">
-          Hi, I'm Damien.
+          <div>Hi, I'm Damien.</div>
+          <div>I'm a software developer specializing in automating marketing strategies and reporting.</div>
         </div>
       </div>
       <div className="text-3xl font-bold">
